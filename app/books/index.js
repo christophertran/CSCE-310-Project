e4fe6bac7af8d887209015fb90f26d1d8b0ff98b
unknown <dortizchaves@outlook.com>
2022-04-20 17:26:37 -0500

    made basic book/id

--- app/books/index.js
+++ app/books/index.js
@@ -1,12 +1,22 @@
+const db = require('../../db');
+
 module.exports = {
     renderBooks: (req, res) => {
         res.render('books/index');
     },
 
     renderBook: (req, res) => {
-        const id = req.params.id;
-        let bookInfo = "Query result from DB here pls";
+        const { id } = req.params;
+        let bookInfo = 'Query result from DB here pls';
 
-        res.render('books/show', {book: bookInfo});
+        db.query('SELECT * FROM books where id = $1;', [id], (err, result) => {
+            if (err) {
+                req.flash('error', `Error fetching book. Error Code: ${err.code}`);
+            }
+
+            bookInfo = result.rows[0];
+
+            return res.render('books/show', { book: bookInfo });
+        });
     },
 };
--- app/landing.js
+++ app/landing.js
@@ -11,28 +11,26 @@ module.exports = {
             max = result.rows[0].count;
 
             console.log(max);
-            let ids = getRandNums(0,max);
+            const ids = getRandNums(0, max);
             console.log(ids);
 
-            db.query('SELECT id, cover FROM books WHERE id IN ($1, $2, $3, $4);', [ids[0], ids[1], ids[2],ids[3]], (err, result) => {
+            db.query('SELECT id, cover FROM books WHERE id IN ($1, $2, $3, $4);', [ids[0], ids[1], ids[2], ids[3]], (err, result) => {
                 if (err) {
                     req.flash('error', `Error fetching books. Error Code: ${err.code}`);
                 }
-                //Debugging tool: return res.json(result);
+                // Debugging tool: return res.json(result);
 
                 return res.render('landing', { data: result.rows });
             });
             // return res.render('landing', { data: result });
         });
-
     },
 
 };
 
-
-function getRandNums(min,max){
-    return [Math.floor(Math.random() * (max - min+1) ) + min,
-            Math.floor(Math.random() * (max - min+1) ) + min,
-            Math.floor(Math.random() * (max - min+1) ) + min,
-            Math.floor(Math.random() * (max - min+1) ) + min]
+function getRandNums(min, max) {
+    return [Math.floor(Math.random() * (max - min + 1)) + min,
+        Math.floor(Math.random() * (max - min + 1)) + min,
+        Math.floor(Math.random() * (max - min + 1)) + min,
+        Math.floor(Math.random() * (max - min + 1)) + min];
 }
